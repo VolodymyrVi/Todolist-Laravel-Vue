1fd69349a95c503005d5d2d6e02283896879d03a
VolodymyrVi <sqlphpjs@gmail.com>
2021-06-08 02:56:46 +0300

    addItem - added reloadlist

--- resources/js/vue/addItemForm.vue
+++ resources/js/vue/addItemForm.vue
@@ -29,6 +29,7 @@ export default {
             .then( response =>{
                 if( response.status == 201 ){
                 this.item.name = "";
+                this.$emit('reloadlist');
                 }
             })
             .catch(error => {
