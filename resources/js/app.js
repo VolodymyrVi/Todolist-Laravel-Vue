f8c55cda9b75fb4836d8ea4e8efef1b3c07e9149
VolodymyrVi <sqlphpjs@gmail.com>
2021-06-08 01:00:27 +0300

    added inport FontAwesomeIcon

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,15 @@
 require('./bootstrap');
 
 import Vue from 'vue'
-
 import App from './vue/app'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 const app = new Vue({
     el: '#app',
